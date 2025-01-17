82408b2640f49c55461c57be9d5d26874e63b9c2
pmagaldi <paulomagaldi@gmail.com>
2025-01-17 19:55:26 -0300

    git inicial do projeto

---
+++ src/main/java/com/alura/chalange/ChalangeApplication.java
@@ -0,0 +1,13 @@
+package com.alura.chalange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChalangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChalangeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alura/chalange/ChalangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.chalange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChalangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
