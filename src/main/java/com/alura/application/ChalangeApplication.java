80681268645a0e41ea1659fbf0c5f195e3f76c12
pmagaldi <paulomagaldi@gmail.com>
2025-01-24 15:51:14 -0300

    resolvendo pacote e arquivos do .idea

--- src/main/java/com/alura/application/ChalangeApplication.java
+++ src/main/java/com/alura/application/ChalangeApplication.java
@@ -1,4 +1,4 @@
-package com.alura.chalange;
+package com.alura.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
