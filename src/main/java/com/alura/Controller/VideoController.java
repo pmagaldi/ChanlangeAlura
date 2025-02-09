90301f912a7393f4800e3da58cd13b84bd7624e7
pmagaldi <paulomagaldi@gmail.com>
2025-02-09 17:42:11 -0300

    criando serviço para salvar vídeo

--- src/main/java/com/alura/Controller/VideoController.java
+++ src/main/java/com/alura/Controller/VideoController.java
@@ -2,6 +2,8 @@ package com.alura.Controller;
 
 import com.alura.DTO.VideoRequest;
 import com.alura.DTO.VideoResponse;
+import com.alura.services.VideoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -12,11 +14,18 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/v1/video")
 public class VideoController {
 
+    private final VideoService videoService;
+
+    @Autowired
+    public VideoController(VideoService videoService) {
+        this.videoService = videoService;
+    }
+
     @PostMapping(consumes = {MediaType.MULTIPART_FORM_DATA_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity<VideoResponse> saveVideo(@RequestBody VideoRequest request) {
         try{
-
-            return ResponseEntity.ok(null);
+            VideoResponse response = videoService.saveDepoimento(request);
+            return ResponseEntity.ok(response);
         } catch (Exception e) {
             throw new RuntimeException();
         }
---
+++ src/main/java/com/alura/services/VideoService.java
@@ -0,0 +1,13 @@
+package com.alura.services;
+
+import com.alura.DTO.VideoRequest;
+import com.alura.DTO.VideoResponse;
+import org.springframework.stereotype.Service;
+
+@Service
+public class VideoService {
+
+    public VideoResponse saveDepoimento(VideoRequest file) {
+        return null;
+    }
+}
